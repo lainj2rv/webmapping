f641c71289bdd3b0e3703d7298e700a5b063a699
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-20 11:16:22 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -98,3 +98,19 @@ pointToLayer: createCircle,
 circles.addTo(map)
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+// add geoJSON layer
+async function addCelltowersGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const markers = L.geoJson(data)
+const clusters = L.markerClusterGroup()
+clusters.addLayer(markers)
+clusters.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+// default map settings
+function defaultMapSettings() {
+map.setView([58.373523, 26.716045], 12)
+}
