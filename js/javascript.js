56166a91491974a77ccb0891ad8611b114eec712
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-20 11:09:55 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -57,3 +57,44 @@ opacity: 1,
 color: 'grey',
 }
 }
+
+async function addDistrictsGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const polygons = L.geoJson(data, {
+onEachFeature: popUPinfo,
+style: polygonStyle,
+})
+polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const markers = L.geoJson(data)
+markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+function createCircle(feature, latlng) {
+let options = {
+radius: 5,
+fillColor: 'red',
+fillOpacity: 0.5,
+color: 'red',
+weight: 1,
+opacity: 1,
+}
+return L.circleMarker(latlng, options)
+}
+async function addCelltowersGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const circles = L.geoJson(data, {
+pointToLayer: createCircle,
+})
+circles.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
