c7a33a094e539ced77def5934858cbb32b37d2c5
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-20 11:34:44 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -7,15 +7,6 @@ attribution: 'OpenStreetMap contributors',})
 
 osm.addTo(map)
 
-// add geoJSON polygons layer*
-async function addDistrictsGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const polygons = L.geoJson(data)
-polygons.addTo(map)
-}
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
-
 // add popup to each feature
 function popUPinfo(feature, layer) {
 layer.bindPopup(feature.properties.NIMI)
@@ -68,15 +59,6 @@ style: polygonStyle,
 polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
-
-// add geoJSON points layer*
-async function addCelltowersGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const markers = L.geoJson(data)
-markers.addTo(map)
-}
-addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
 function createCircle(feature, latlng) {
 let options = {
