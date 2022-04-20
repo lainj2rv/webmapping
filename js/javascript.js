0a9a95e174a6b9a8e645b406d544c4902736d906
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-20 11:07:23 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,54 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 attribution: 'OpenStreetMap contributors',})
 
 osm.addTo(map)
+
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const polygons = L.geoJson(data)
+polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+layer.bindPopup(feature.properties.NIMI)
+}
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+const response = await fetch(url)
+const data = await response.json()
+const polygons = L.geoJson(data, {
+onEachFeature: popUPinfo,
+})
+polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// get color from feature property
+function getColor(property) {
+switch (property) {
+case 1:
+return '#ff0000'
+case 13:
+return '#009933'
+case 6:
+return '#0000ff'
+case 7:
+return '#ff0066'
+default:
+return '#ffffff'
+}
+}
+
+// polygon style
+function polygonStyle(feature) {
+return {
+fillColor: getColor(feature.properties.OBJECTID),
+fillOpacity: 0.5,
+weight: 1,
+opacity: 1,
+color: 'grey',
+}
+}
