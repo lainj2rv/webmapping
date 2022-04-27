ac9b1453a5770115fc6494f78546f9c0fd4d71f5
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-27 10:59:04 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -11,9 +11,19 @@ osm.addTo(map)
 async function addCelltowersGeoJson(url) {
 const response = await fetch(url)
 const data = await response.json()
-console.log(data.features[0])
+const heatData = data.features.map(heatDataConvert)
+const heatMap = L.heatLayer(heatData, { radius: 10 })
+heatMap.addTo(map)
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+function heatDataConvert(feature) {
+return [
+feature.geometry.coordinates[1],
+feature.geometry.coordinates[0],
+feature.properties.area,
+]
+}
 
 // default map settings
 function defaultMapSettings() {
