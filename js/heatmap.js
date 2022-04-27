bac3bc0de684fb222724c0e728260d9bc3c5101d
lainj2rv <104059175+lainj2rv@users.noreply.github.com>
2022-04-27 10:55:38 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -3,60 +3,15 @@ let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
             {maxZoom: 19,
-attribution: 'OpenStreetMap contributors',})
+             attribution: 'OpenStreetMap contributors',})
 
 osm.addTo(map)
-
-// add popup to each feature
-function popUPinfo(feature, layer) {
-layer.bindPopup(feature.properties.NIMI)
-}
-
-// polygon style
-function polygonStyle(feature) {
-return {
-fillColor: '#fdfd96',
-fillOpacity: 0.5,
-weight: 1,
-opacity: 1,
-color: '#023020',
-}
-}
-
-// add geoJSON polygons layer
-async function addDistrictsGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-const polygons = L.geoJson(data, {
-onEachFeature: popUPinfo,
-style: polygonStyle,
-})
-polygons.addTo(map)
-}
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
-
-function createCircle(feature, latlng) {
-let options = {
-radius: 5,
-fillColor: '#083461',
-fillOpacity: 0.8,
-color: '#083461',
-weight: 1,
-opacity: 1,
-}
-return L.circleMarker(latlng, options)
-}
 
 // add geoJSON layer
 async function addCelltowersGeoJson(url) {
 const response = await fetch(url)
 const data = await response.json()
-const circles = L.geoJson(data, {
-pointToLayer: createCircle,
-})
-const clusters = L.markerClusterGroup()
-clusters.addLayer(circles)
-clusters.addTo(map)
+console.log(data.features[0])
 }
 addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
 
